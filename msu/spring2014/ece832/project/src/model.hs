d0ce7fdf49be8f5584f759a6447a13f6d54ebf07
Sam Boling <charles.samuel.boling@gmail.com>
2014-04-08 22:39:21 -0400

    added ece875 hw8

--- msu/spring2014/ece832/project/src/model.hs
+++ msu/spring2014/ece832/project/src/model.hs
@@ -1,22 +1,17 @@
 import Graphics.EasyPlot
 import Data.Bits
+import Control.Arrow
 
-sarADC :: (Enum b, Ord b, Floating b) => b -> (a -> b) -> a -> b
-sarADC depth f t = foldl comp 0 [1..depth] 
-                   where comp v i = if v + bit i < x then v + bit i else v
+sarADC :: (Enum b, Ord b, Floating b) => b -> (a -> b) -> a -> (b, [Bool])
+sarADC depth f t = fst . last &&& map snd . tail $ scanl comp (0, False) [1..depth] 
+                   where comp (v, _) i = let next = v + bit i in
+                                         (if next < x then next else v, next < x)
                          bit i    = 2**(depth-i)
                          x        = 2**depth * f t
 
 heaviside x
   | x < 0     = 0
   | otherwise = 1
-
-alt depth f t = foldr comp (0, []) [0..depth-1]
-                where comp i (v, bits) = let next = v + 2**i
-                                             pred = next < x in 
-                                             (if pred then next else v, pred : bits)
-                      x = 2**depth * f t
-                      
 
 {-alternative depth f t = accum step 
               where accum term = sum $ map (\i -> term * 2**(depth-i)) [1..depth]
@@ -27,12 +22,12 @@ alt depth f t = foldr comp (0, []) [0..depth-1]
 lfsr taps bits = shiftL 1 bits .|. ones `mod` 2 
                  where ones = popCount (bits .&. taps) + 1
 
-bitdepth = 8
+bitdepth = 2
 freq     = 0.25
 input    = (/2) . (1+) . sin . (2 * pi * freq *)
 main = do
        plot X11 [ subtract 0.5 . (2**bitdepth *) . input
-                , fst . alt bitdepth input
-            --    , sarADC bitdepth input
+--                , fst . alt bitdepth input
+                , fst . sarADC bitdepth input
                 ]
 
