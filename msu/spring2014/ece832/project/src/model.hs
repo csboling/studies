caaad2609a2f9ae0f26e2c4f1c215f70a02966e8
cboling <charles.samuel.boling@gmail.com>
2014-04-11 20:17:58 -0400

    trying again

--- msu/spring2014/ece832/project/src/model.hs
+++ msu/spring2014/ece832/project/src/model.hs
@@ -1,6 +1,22 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
 import Graphics.EasyPlot
+
+import Data.Conduit
+import Data.Conduit.Binary
+import qualified Data.Conduit.List as CL
+import Data.CSV.Conduit
+import Data.Traversable
+
+import Data.Text (Text, pack, unpack)
 import Data.Bits
+
+import Control.Monad.IO.Class (liftIO)
 import Control.Arrow
+
+
+lfsr taps bits = shiftL 1 bits .|. ones `mod` 2 
+                 where ones = popCount (bits .&. taps) + 1
 
 sarADC :: (Enum b, Ord b, Floating b) => b -> (a -> b) -> a -> (b, [Bool])
 sarADC depth f t = fst . last &&& map snd . tail $ scanl comp (0, False) [1..depth] 
@@ -10,24 +26,45 @@ sarADC depth f t = fst . last &&& map snd . tail $ scanl comp (0, False) [1..dep
                          x        = 2**depth * f t
 
 heaviside x
-  | x < 0     = 0
+  | x <= 0    = 0
   | otherwise = 1
 
-{-alternative depth f t = accum step 
-              where accum term = sum $ map (\i -> term * 2**(depth-i)) [1..depth]
-                    step       = heaviside (x - accum 1)
-                    x          = f $! t
--}
+decode :: (RealFrac a, Num b) => [a] -> b
+decode bits = sum $ zipWith ((*) . heaviside . round) bits weights
+              where weights = [2^(depth-i) | i <- [1..depth]]
+                    depth   = length bits
 
-lfsr taps bits = shiftL 1 bits .|. ones `mod` 2 
-                 where ones = popCount (bits .&. taps) + 1
+toPulses timescale vdd (t:vs) = (lift (timescale *) t):(map (lift (vdd*)) vs)
+                              where lift f = pack . show . f . read . unpack
+
+hz    = 10e3
+scale :: Monad m => Conduit (Row Text) m (Row Text)
+scale = CL.map (toPulses (1/hz * 1/20e-12) 3.0)
+
+getColumn n = CL.mapM ((:[]) . (!! n))
+
+split = getZipSink $ traverse (ZipSink . splitter) [0..12]
+        where splitter n = getColumn n  =$ fromCSV defCSVSettings -- $$ sinkFile ((show n) ++ ".txt")
+              
+
+toPWLs = fromCSV defCSVSettings =$ toFiles
+         where toFiles = getZipSink $ traverse (ZipSink . sinkFile) (map ((++".txt") . show) [0..12])
+
+
 
 bitdepth = 2
 freq     = 0.25
 input    = (/2) . (1+) . sin . (2 * pi * freq *)
-main = do
-       plot X11 [ subtract 0.5 . (2**bitdepth *) . input
---                , fst . alt bitdepth input
-                , fst . sarADC bitdepth input
-                ]
+main = runResourceT $
+         sourceFile "../example-pulses.csv" $=
+         intoCSV defCSVSettings             $=
+         scale                              $$
+--         fromCSV defCSVSettings             $$
+--         split 
+         toPWLs                           
+--         sinkFile "blah.pwl"         
+--         intoCSV defCSVSettings $=
+--       plot X11 [ subtract 0.5 . (2**bitdepth *) . input
+--                , fst . sarADC bitdepth input
+--                ] 
 
