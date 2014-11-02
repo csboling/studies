200106844db760aff84e73ce18a4f1c8fb033e24
Sam Boling <charles.samuel.boling@gmail.com>
2014-11-02 13:29:06 -0500

    conjugation problem done

--- msu/fall2014/math818/hw/hw1/src/gen.hs
+++ msu/fall2014/math818/hw/hw1/src/gen.hs
@@ -10,10 +10,13 @@ generate m n = (join $ iterate . plusMod m) *** (join $ iterate . plusMod n)
 
 cycles m n = map (uncurry zip . generate m n) as
   where as = (,) <$> [0..m-1] <*> [0..n-1]
-           
+
 generators m n = map results $ cycles m n
                  where results []     = []
                        results (x:xs) = x : (takeWhile (/= x) xs)
 
-main = mapM_ (putStrLn . show) $ generators 5 5
+disjointPairs n = filter (uncurry (/=)) $ (,) <$> [0..n-1] <*> [0..n-1]
+orbit f x = x : (takeWhile (/= x) . tail $ iterate f x)
+getOrbits n = map (orbit (join (***) $ plusMod n 1)) $ disjointPairs n
 
+main = mapM_ (putStrLn . show) $ generators 5 5
