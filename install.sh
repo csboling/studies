cbdc64cee87843764cc68aaf8ee29006a266b452
Sam Boling <charles.samuel.boling@gmail.com>
2014-11-08 17:58:33 -0500

    installation script fixed

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-DESTINATION=`realpath $1`
+# Create symbolic links from many directories into the current directory,
+# which should have the same internal directory structure. This was created
+# to allow my public schoolwork repository to contain notes etc, while still
+# referring to sensitive material such as exam solutions in a private 
+# repo which lives in a submodule.
+DESTINATION=`realpath $1 --relative-to=.`
 KEEP=( 
        'hw'
        'exams'
@@ -9,14 +14,16 @@ KEEP=(
 for x in `find . \! \( ${KEEP[@]/#/-not -name } \) | cut -c3-`
 do
   TO_ADD=$DESTINATION/$x
-  if [ -f $TO_ADD ]
+  if [ -e $TO_ADD ]
   then
     echo 'Skipped existing file' $TO_ADD
+    continue
   fi
   if [ ! -h $TO_ADD ] 
   then
-    echo `readlink -f $x`
-    ln -s `readlink -f $x` $TO_ADD
+    LINKPOINT=`dirname $TO_ADD`
+    echo 'linkpoint' $LINKPOINT
+    ln -s `realpath $x --relative-to=$LINKPOINT` $TO_ADD
     echo $TO_ADD
   fi
 done
